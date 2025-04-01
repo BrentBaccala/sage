4a57ef6a5c1fcc097287db363c64a29f6b382d93
Brent Baccala <cosine@freesoft.org>
2025-03-31 23:18:59 -0400

    invoke prange the way the docs say to do it

--- src/sage/rings/polynomial/multi_polynomial_ideal_libsingular.pyx
+++ src/sage/rings/polynomial/multi_polynomial_ideal_libsingular.pyx
@@ -50,7 +50,7 @@ Two examples from the Mathematica documentation (done in Sage):
 
 from cysignals.signals cimport sig_on, sig_off
 
-cimport cython
+from cython.parallel import prange
 
 from sage.libs.singular.decl cimport tHomog, number, IDELEMS, p_Copy, rChangeCurrRing
 from sage.libs.singular.decl cimport idInit, id_Delete, currRing, Sy_bit, OPT_REDSB
@@ -459,7 +459,7 @@ def simplifyIdeal_libsingular(gens):
                 else:
                     if subst_var != -1:
                         simplifications.append(new_MP(R, p))
-                        for j in cython.parallel.prange(ngens):
+                        for j in prange(ngens, nogil=True):
                             p = pgens[j]
                             if p:
                                 p = p_Copy(p, r)
@@ -512,7 +512,7 @@ def simplifyIdeal_libsingular(gens):
                             sp = p_Div_nn(sp, coeff, r)
                             sp = p_Neg(sp, r)
                             simplifications.append(new_MP(R, p))
-                            for j in cython.parallel.prange(ngens):
+                            for j in prange(ngens, nogil=True):
                                 p = pgens[j]
                                 if p:
                                     p = p_Copy(p, r)
@@ -592,7 +592,7 @@ def simplifyIdeal_libsingular(gens):
 
                             from_id = idInit(1, 1)
 
-                            for j in cython.parallel.prange(ngens):
+                            for j in prange(ngens, nogil=True):
                                 p = pgens[j]
                                 if p:
                                     # singular_polynomial_subst calls pSubst, which only works for monomials
